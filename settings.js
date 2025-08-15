8aca37e0d482b4b1612204a1ce80c4da81575539
twenty010 <dghcdkhxbv@gmail.com>
2025-08-16 02:21:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['967737088693'] //['628','628'] 2 owner atau lebih
-global.author = 'TWENTY'
-global.botname = 'TWENTY Bot'
+global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
+global.author = 'Nazedev'
+global.botname = 'Hitori Bot'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
 
@@ -37,7 +37,7 @@ global.fake = {
 
 global.my = {
 	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/twenty010',
+	gh: 'https://github.com/nazedev',
 	gc: 'https://chat.whatsapp.com/B5qJIwZHm4VEYZJQE6iMwy',
 	ch: '120363250409960161@newsletter',
 }
